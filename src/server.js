ad0d761dba274e136d1a1c2fb6912d1f4ecffd75
EDUNI11 <edunievasdris@gmail.com>
2024-11-18 09:05:13 +0100

    Get video names

--- src/server.js
+++ src/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const port = 42069;
+const port = 3333;
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(port, () => {
-    console.log(`Servidor engegat en el port : ${port}`);
+    console.log(`Server is running in port: ${port}`);
 });
+
+app.get('/video/getAllVideos', async(req,res) => {
+    try {
+        const videoDir = path.join(__dirname, '../videos');
+        const videosSource = fs.readdirSync(videoDir);
+
+        const videoJson = videosSource.map(video => {
+            const videoName = video.split('.')[0];
+            return {
+                name: videoName,
+            };
+        });
+
+        res.json(videoJson);
+    } catch (error) {
+        console.log('Error reading videos: ', error);
+    }
+})
+
 
 app.get('/video/getvideo', async(req, res) => {
 
