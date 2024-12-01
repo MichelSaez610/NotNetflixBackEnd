cd196c7850208cfc55adaa96505be11b921431cc
EDUNI11 <edunievasdris@gmail.com>
2024-12-01 01:37:25 +0100

    Finished

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
@@ -11,43 +10,57 @@ const port = 3333;
 app.use(express.json());
 app.use(cors());
 
+const videosPath = path.join(__dirname, '../videos');
+
+app.use('/videos', express.static(videosPath));
+
+
 const io = socketIo(app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 }), {
     cors: {
-        origin: 'http://localhost:4200', // Allowing connection from Angular client
+        origin: 'http://localhost:4200',
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type'],
         credentials: true
     }
 });
 
-var client_choice = ""
-var genCode = ""
+let generatedCode = '';
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('A user connected:', socket.id);
 
-    socket.on('message', (msg) => {
-        console.log('Received message from client:', msg);
-        io.emit('message', msg); 
+    socket.on('message', (selectedVideo) => {
+        console.log(`Received message from client (${socket.id}):`, selectedVideo);
 
-        client_choice = msg
-        genCode = randomCode();
-        console.log('client choice: ' + client_choice);
-        console.log('Generated code: ' + genCode);
+        generatedCode = randomCode();
+        console.log('Generated code:', generatedCode);
 
-        socket.emit('generatedCode', genCode);
+        io.emit('generatedCode', generatedCode);
+    });
+
+    socket.on('validateCode', (userCode) => {
+        let isValid = false;
+
+        if (userCode === generatedCode) {
+            console.log('Code is valid');
+            isValid = true;
+        } else {
+            console.log('Invalid code');
+        }
+        io.emit('codeValidationResult', { valid: isValid });
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('User disconnected:', socket.id);
     });
 
     socket.on('connect_error', (err) => {
         console.log(`Connection error: ${err.message}`);
     });
 });
+
 
 app.get('/api/video/getAllVideos', async (req, res) => {
     try {
@@ -82,7 +95,7 @@ function randomCode() {
     let counter = 0
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     const charLength = characters.length
-    while (counter < 4) {
+    while (counter < 8) {
         string += characters.charAt(Math.floor(Math.random() * charLength));
         counter += 1;
     }
